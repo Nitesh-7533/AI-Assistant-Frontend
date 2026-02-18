0b7db7d3a4b461b3a8c62f47506c3b60aca8589c
Nitesh kumar <nkumar49575@gmail.com>
2026-02-18 14:15:56 +0530

    Fix import path for userDataContext

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,7 +3,7 @@ import authBg from "../assets/authBg.png"
 import { IoEye } from "react-icons/io5";
 import { IoEyeOff } from "react-icons/io5";
 import { Navigate, useNavigate } from 'react-router-dom';
-import { userDataContext } from '../context/userContext';
+import { userDataContext } from '../context/UserContext';
 import axios from "axios"
 
 
